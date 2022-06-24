3f495310498d36255b5c62c3200ace5ae7c4d47d
Jeremias Leao <jeremaios@gmail.com>
2022-06-24 11:40:17 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi {process.env.REACT_APP_NAME}
         </p>
         <a
           className="App-link"
